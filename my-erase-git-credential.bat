9229f6a769483861673ea6bf3ecda980c717c06f
CppCommons <cppcommons@gmail.com>
2017-10-20 14:20:13 +0900

    .

--- my-erase-git-credential.bat
+++ my-erase-git-credential.bat
@@ -1,5 +1,3 @@
 echo host=github.com
 echo protocol=https
-::E:\cmder@javacommons\vendor\git-for-windows\mingw32\libexec\git-core\git-credential-wincred.exe erase
 %ConEmuBaseDir%\..\..\..\vendor\git-for-windows\mingw32\libexec\git-core\git-credential-wincred.exe erase
-
