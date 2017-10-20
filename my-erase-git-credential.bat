ea466bd105baabb4b698f8f8a04d2e559dd91edf
JavaCommons <javacommons@gmail.com>
2017-10-20 14:19:16 +0900

    .

---
+++ my-erase-git-credential.bat
@@ -0,0 +1,5 @@
+echo host=github.com
+echo protocol=https
+::E:\cmder@javacommons\vendor\git-for-windows\mingw32\libexec\git-core\git-credential-wincred.exe erase
+%ConEmuBaseDir%\..\..\..\vendor\git-for-windows\mingw32\libexec\git-core\git-credential-wincred.exe erase
+
